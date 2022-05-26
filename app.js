8bf29f2365e70c7056f8c3828eceb95f0b9011d3
Lucas <lucas1721@hotmail.com>
2022-05-26 18:06:11 -0300

    atualização teste

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
-const res = require("express/lib/response");
+// const res = require("express/lib/response");
 
 let app = express();
 //Antes de criar o servidor precisamos criar uma rota:
-app.get("/", (req, res) => res.send("Olá mundo"));
+
+app.post("/", (req, res) => res.send("Olá mundo"));
+app.get("/contatos", (req, res) => res.send({ nome: "Lucas", idade: 26 }));
+
+let produto = {
+  tipoProduto: null,
+  preco: null,
+  quantidade: null,
+};
+
+app.post("/produto/criar", (req, res) => {
+  (produto.tipoProduto = "living"),
+    (produto.preco = 1245),
+    (produto.quantidade = 300);
+  res.send(produto);
+});
 
 //criando um servidor com o express
 app.listen(3000, () => console.log("O servidor está rodando na porta 3000"));
--- teste.js
+++ teste.js
@@ -1,3 +1,15 @@
-let teste = 7;
+let dia = "sexta";
 
-teste = "7";
+function fimDeSemana(dia) {
+  switch (dia) {
+    case "segunda":
+    case " quarta":
+    case "sexta-feira":
+      console.log(" você tem aulas");
+      break;
+    default:
+      console.log("você não tem aulas");
+  }
+}
+
+fimDeSemana(dia);
